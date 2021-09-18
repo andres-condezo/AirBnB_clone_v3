50a0ae9141e07f5dd4b2fa592a9ab61f3c164065
andres-condezo <ra.condezomonge@gmail.com>
2021-09-18 16:30:18 -0500

    Fix indentatiosn

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,10 +21,10 @@ def stats():
     """Retrieves the number of each objects by type"""
 
     stat = {"amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")}
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")}
 
     return jsonify(stat)
