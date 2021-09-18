3e4f18a494f2c70ea5ae9a78b2d8c2f7274e4cac
andres-condezo <ra.condezomonge@gmail.com>
2021-09-18 14:10:24 -0500

    Create stats end point

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,4 +12,19 @@ from models import storage
 @app_views.route('/status')
 def status():
     """ returns a JSON """
-    return jsonify({'status': 'OK'})
+    response = {'status': 'OK'}
+    return jsonify(response)
+
+
+@app_views.route('/stats')
+def stats():
+    """Retrieves the number of each objects by type"""
+
+    stat = {"amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")}
+
+    return jsonify(stat)
